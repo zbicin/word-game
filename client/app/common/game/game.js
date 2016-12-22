5d3f94505a069f5dfcf5dfaa34cf72df7460cc68
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2016-12-22 17:25:38 +0100

    Add Helpers dep to Game and WordBank

--- client/app/common/game/game.js
+++ client/app/common/game/game.js
@@ -1,7 +1,8 @@
 import angular from 'angular';
+import helpersModule from '../helpers/helpers';
 import GameService from './game.service';
 
-let gameModule = angular.module('game', [])
+let gameModule = angular.module('game', [helpersModule])
 
 .service('Game', GameService)
 
--- client/app/common/word-bank/word-bank.js
+++ client/app/common/word-bank/word-bank.js
@@ -1,7 +1,8 @@
 import angular from 'angular';
+import helpersModule from '../helpers/helpers';
 import WordBankService from './word-bank.service';
 
-let wordBankModule = angular.module('wordBank', [])
+let wordBankModule = angular.module('wordBank', [helpersModule])
 
 .service('WordBank', WordBankService)
 
