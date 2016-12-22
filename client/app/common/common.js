d792636e641cdb4c8e5a71254d145b6358c7020f
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2016-12-22 16:17:37 +0100

    Remove Hero and User which are no longer used

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,13 +1,9 @@
 import angular from 'angular';
 import Navbar from './navbar/navbar';
-import Hero from './hero/hero';
-import User from './user/user';
 import Player from './player/player';
 
 let commonModule = angular.module('app.common', [
   Navbar,
-  Hero,
-  User,
   Player
 ])
   
