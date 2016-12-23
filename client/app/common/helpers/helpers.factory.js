c21112c23ea6e7ba56b515f63aed76ae08b36b78
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2016-12-23 16:47:37 +0100

    TODO do not rely on Math.random in shuffleArray

--- client/app/common/helpers/helpers.factory.js
+++ client/app/common/helpers/helpers.factory.js
@@ -9,6 +9,7 @@ let HelpersFactory = () => {
 
       // adapted from http://stackoverflow.com/a/6274381/6696238
       for (let i = result.length; i; i--) {
+        // TODO: do not rely only on Math.random which can provide a few same results in a row
         let j = Math.floor(Math.random() * i);
         // elements swap
         [result[i - 1], result[j]] = [result[j], result[i - 1]];
