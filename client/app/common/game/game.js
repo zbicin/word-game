a2e3f9eafda12211db0f2a26348f346511e886ff
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2016-12-23 11:54:18 +0100

    Fix missing game.js module deps

--- client/app/common/game/game.js
+++ client/app/common/game/game.js
@@ -1,8 +1,14 @@
 import angular from 'angular';
+import globalEventsModule from '../global-events/global-events';
 import helpersModule from '../helpers/helpers';
+import wordBankModule from '../word-bank/word-bank';
 import GameService from './game.service';
 
-let gameModule = angular.module('game', [helpersModule])
+let gameModule = angular.module('game', [
+    globalEventsModule,
+    helpersModule,
+    wordBankModule
+])
 
 .service('Game', GameService)
 
