c6195a496f05df73d856fb7a4635f5ed1e67c1e3
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2016-12-23 12:18:54 +0100

    Remove About component

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,12 +1,10 @@
 import angular from 'angular';
 import Home from './home/home';
-import About from './about/about';
 import Gameboard from './gameboard/gameboard';
 import Scoreboard from './scoreboard/scoreboard';
 
 let componentModule = angular.module('app.components', [
   Home,
-  About,
   Gameboard,
   Scoreboard
 ])
